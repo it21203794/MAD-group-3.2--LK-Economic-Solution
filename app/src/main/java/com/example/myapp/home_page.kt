64c7b064006aa1485d6c28b0d39a968c563e79d8
chameenshamika <chameenshamika@gmail.com>
2023-05-06 16:21:21 +0530

    adding hame page

---
+++ app/src/main/java/com/example/myapp/home_page.kt
@@ -0,0 +1,11 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_page)
+    }
+}
