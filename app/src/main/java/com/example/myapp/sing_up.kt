6cebd4880ef0b803f14d904da0c0a29dea6ca743
chameenshamika <chameenshamika@gmail.com>
2023-05-16 14:48:57 +0530

    comment sing_up.kt

--- app/src/main/java/com/example/myapp/sing_up.kt
+++ app/src/main/java/com/example/myapp/sing_up.kt
@@ -17,36 +17,46 @@ class sing_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inflate the layout using the generated binding class
         binding = SingUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        // Set a click listener for the "Sign In" textView
         binding.textView.setOnClickListener {
             val intent = Intent(this, sign_in::class.java)
             startActivity(intent)
         }
+        // Set a click listener for the "Sign Up" button
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
 
+            // Check if all fields are non-empty
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                // Check if the password matches the confirmed password
                 if (pass == confirmPass) {
 
+                    // Create a user with the provided email and password using FirebaseAuth
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                         if (it.isSuccessful) {
+                            // If user creation is successful, navigate to the sign-in activity
                             val intent = Intent(this, sign_in::class.java)
                             startActivity(intent)
                         } else {
+                            // If user creation fails, display the exception message as a Toast
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
                         }
                     }
                 } else {
+                    // If the password and confirmed password do not match, display an error Toast
                     Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
                 }
             } else {
+                // If any of the fields are empty, display an error Toast
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
 
             }
