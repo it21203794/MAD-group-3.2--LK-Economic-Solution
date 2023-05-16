210244bb28e1989ab8655fa7639878da9cdc88ec
chameenshamika <chameenshamika@gmail.com>
2023-05-16 15:00:12 +0530

    comment f_p.kt

--- app/src/main/java/com/example/myapp/find_project.kt
+++ app/src/main/java/com/example/myapp/find_project.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 
 class find_project : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.find_project)
