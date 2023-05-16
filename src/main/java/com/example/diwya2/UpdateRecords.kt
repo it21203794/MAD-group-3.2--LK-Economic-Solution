09a6637f5fcd1aa08d30be046dae8ae6add557d2
IT21183218 <99754850+IT21183218@users.noreply.github.com>
2023-05-16 15:34:33 +0530

    diwya

--- src/main/java/com/example/diwya2/UpdateRecords.kt
+++ src/main/java/com/example/diwya2/UpdateRecords.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class UpdateRecords : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_records)
     }
