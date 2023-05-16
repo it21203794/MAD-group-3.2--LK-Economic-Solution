94156d5ee4a09b4026e3bac35c33c6069674cbf7
chameenshamika <chameenshamika@gmail.com>
2023-05-16 14:39:02 +0530

    comment g.kt

--- app/src/main/java/com/example/myapp/models/Gig.kt
+++ app/src/main/java/com/example/myapp/models/Gig.kt
@@ -1,5 +1,5 @@
 package com.example.myapp.models
 
-data class Gig(val gigId: String? =null,val category : String?= null, val mainSkill : String?= null){
+data class Gig(val gigId: String? =null,val category : String?= null, val mainSkill : String?= null, val about12 : String?= null){
 
 }
