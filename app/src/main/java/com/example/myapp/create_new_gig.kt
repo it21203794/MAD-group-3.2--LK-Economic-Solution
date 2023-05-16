077b059a02bcb7e7b9e0efbb601209fd8b06c822
chameenshamika <chameenshamika@gmail.com>
2023-05-16 14:35:19 +0530

    comment c_n_g

--- app/src/main/java/com/example/myapp/create_new_gig.kt
+++ app/src/main/java/com/example/myapp/create_new_gig.kt
@@ -12,55 +12,76 @@ import com.google.firebase.database.FirebaseDatabase
 
 class create_new_gig : AppCompatActivity() {
 
+    // Declare the EditText fields and ImageButton
     private lateinit var category : EditText
     private lateinit var mainSkill : EditText
+    private lateinit var about12 : EditText
+
     private lateinit var btnCreate : ImageButton
 
+    // Declare the database reference
     private lateinit var dbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_new_gig)
 
+        // Initialize the EditText fields and ImageButton
         category = findViewById(R.id.category)
         mainSkill = findViewById(R.id.mainSkill)
+        about12 = findViewById(R.id.about12)
         btnCreate = findViewById(R.id.btnCreate)
 
+        // Get the Firebase database reference for the "Gig" node
         dbRef = FirebaseDatabase.getInstance().getReference("Gig")
 
+        // Set a click listener for the create button
         btnCreate.setOnClickListener {
             saveGigData()
         }
 
     }
+    // Function to save gig data to Firebase database
     private fun saveGigData(){
+        // Get the text values from the EditText fields
         val qCategory = category.text.toString()
         val qMainSkill = mainSkill.text.toString()
+        val qAbout12 = about12.text.toString()
 
+        // Validate the input fields and show errors if empty
         if(qCategory.isEmpty()){
             category.error = "Please enter category"
         }
         if(qMainSkill.isEmpty()){
-            mainSkill.error = "Please enter category"
+            mainSkill.error = "Please enter mainSkill"
+        }
+        if(qAbout12.isEmpty()){
+            about12.error = "Please enter about"
         }
 
+        // Generate a unique gigId using push().key!!
         val gigId = dbRef.push().key!!
+        // Create a Gig object with the entered data
 
-        val gig = Gig(gigId,qCategory,qMainSkill)
+        val gig = Gig(gigId,qCategory,qMainSkill,qAbout12)
 
+        // Save the gig data to the database
         dbRef.child(gigId).setValue(gig)
             .addOnCompleteListener{
                 Toast.makeText(this,"insert data successfully", Toast.LENGTH_LONG).show()
+                // Clear the EditText fields after successful data insertion
                 category.text.clear()
                 mainSkill.text.clear()
+                about12.text.clear()
             }.addOnFailureListener{err ->
                 Toast.makeText(this,"Error ${err.message}",Toast.LENGTH_LONG).show()
             }
 
 
-
+        // Set a click listener for the ImageButton
         var imageView41 = findViewById<ImageButton>(R.id.imageView41)
         imageView41.setOnClickListener{
+            // Start the find_freelancers activity
             val intent = Intent(this, find_freelancers::class.java)
             startActivity(intent)
 
