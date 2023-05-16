ec53892c4ca1fa73721207bc93a48e7d0421e6d6
chameenshamika <chameenshamika@gmail.com>
2023-05-16 14:42:27 +0530

    comment p_a_p.kt

--- app/src/main/java/com/example/myapp/post_a_project.kt
+++ app/src/main/java/com/example/myapp/post_a_project.kt
@@ -27,6 +27,7 @@ class post_a_project : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.post_a_project)
 
+        // Find the ImageButton and set a click listener to navigate to create_new_project activity
         var imageButton = findViewById<ImageButton>(R.id.imageButton)
         imageButton.setOnClickListener {
             val intent = Intent(this, create_new_project::class.java)
@@ -34,6 +35,7 @@ class post_a_project : AppCompatActivity() {
 
         }
 
+        // Initialize RecyclerView, TextView, and projectList
             projectRecyclerView = findViewById(R.id.projectList)
             rtvLoadingData = findViewById(R.id.rtvLoadingData)
             projectRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -41,28 +43,37 @@ class post_a_project : AppCompatActivity() {
 
             projectList = arrayListOf<Project>()
 
+        // Fetch projects data from Firebase Realtime Database
             getProjectsData()
 
         }
         private fun getProjectsData(){
+            // Hide the projectRecyclerView and show the loading text while fetching data
             projectRecyclerView.visibility = View.GONE
             rtvLoadingData.visibility = View.VISIBLE
 
+            // Get a reference to the "Project" node in the Firebase Realtime Database
             dbRef = FirebaseDatabase.getInstance().getReference("Project")
+
+            // Add a ValueEventListener to listen for changes in the data at the "Project" node
 
             dbRef.addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
+                    // Clear the projectList before adding new data
                     projectList.clear()
                     if (snapshot.exists()){
+                        // Iterate through the children of the snapshot to retrieve project data
                         for (projectSnap in snapshot.children){
                             val projectData = projectSnap.getValue(Project::class.java)
                             projectList.add(projectData!!)
                         }
+                        // Create an instance of the ProjectAdapter and set it as the adapter for the projectRecyclerView
                         val mAdapter = ProjectAdapter(projectList)
                         projectRecyclerView.adapter = mAdapter
 
                         mAdapter.setOnItemClickListener(object : ProjectAdapter.onItemClickListener{
                             override fun onItemClick(position: Int) {
+                                // Create an intent to navigate to project_view activity and pass relevant data as extras
                                 val intent = Intent(this@post_a_project, project_view::class.java)
                                 //put extras
                                 intent.putExtra("projectId", projectList[position].projectId)
@@ -76,13 +87,14 @@ class post_a_project : AppCompatActivity() {
                         })
 
 
-
+                        // Show the projectRecyclerView and hide the loading text after fetching and setting the data
                         projectRecyclerView.visibility = View.VISIBLE
                         rtvLoadingData.visibility = View.GONE
                     }
                 }
 
                 override fun onCancelled(error: DatabaseError) {
+                    // Handle the onCancelled event if it occurs
                     TODO("Not yet implemented")
                 }
 
