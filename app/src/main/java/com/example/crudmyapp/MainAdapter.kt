bdd440005cf16cc7128b6e0e25df646c02d59f84
Kavindu Naveen <it21226878@my.sliit.lk>
2023-05-16 15:07:02 +0530

    kavindu

--- app/src/main/java/com/example/crudmyapp/MainAdapter.kt
+++ app/src/main/java/com/example/crudmyapp/MainAdapter.kt
@@ -10,7 +10,6 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.crudmyapp.MainAdapter.myViewHolder
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
@@ -19,6 +18,8 @@ import com.orhanobut.dialogplus.DialogPlus
 import com.orhanobut.dialogplus.ViewHolder
 import de.hdodenhof.circleimageview.CircleImageView
 
+
+// Class declaration
 class MainAdapter
 /**
  * Initialize a [RecyclerView.Adapter] that listens to a Firebase query. See
@@ -28,56 +29,63 @@ class MainAdapter
  */
     (options: FirebaseRecyclerOptions<MainModel?>) :
     FirebaseRecyclerAdapter<MainModel, myViewHolder>(options) {
+
+    // Method to bind data to the view holder
     override fun onBindViewHolder(
         holder: myViewHolder,
         @SuppressLint("RecyclerView") position: Int,
         model: MainModel
     ) {
-        holder.itemName.text = model.itemName
+        holder.name.text = model.itemName
         holder.price.text = model.price
         holder.description.text = model.description
-        Glide.with(holder.img.context)
-            .load(model.iImage)
-            .placeholder(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark)
-            .circleCrop()
-            .error(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark_normal)
-            .into(holder.img)
-        holder.btnEdit.setOnClickListener {
+
+        // Set click listener for the btnEdit
+        holder.btnEdit.setOnClickListener { view ->
             val dialogPlus = DialogPlus.newDialog(holder.img.context)
                 .setContentHolder(ViewHolder(R.layout.update_popup))
                 .setExpanded(true, 1200)
                 .create()
 
-            dialogPlus.show();
-            val view = dialogPlus.footerView
-            val itemName = view.findViewById<EditText>(R.id.txtItemName)
-            val price = view.findViewById<EditText>(R.id.txtPrice)
-            val description = view.findViewById<EditText>(R.id.txtDescription)
-            val iImage = view.findViewById<EditText>(R.id.txtImageUrl)
+            //dialogPlus.show();
+
+            // Get references to the views
+            val headerView = dialogPlus.headerView
+            val name = headerView.findViewById<EditText>(R.id.txtItemName)
+
+            val price = headerView.findViewById<EditText>(R.id.txtPrice)
+            val description = headerView.findViewById<EditText>(R.id.txtDescription)
             val btnUpdate = view.findViewById<Button>(R.id.btnUpdate)
-            itemName.setText(model.itemName)
+
+            // Set the current values in the update popup
+            name.setText(model.itemName)
             price.setText(model.price)
             description.setText(model.description)
-            iImage.setText(model.iImage)
+
+
             dialogPlus.show()
+
+            // Set click listener for the btnUpdate
             btnUpdate.setOnClickListener {
                 val map: MutableMap<String, Any> = HashMap()
-                map["itemName"] = itemName.text.toString()
+                map["itemName"] = name.text.toString()
                 map["price"] = price.text.toString()
                 map["description"] = description.text.toString()
+
+                // Update the data in the Firebase database
                 FirebaseDatabase.getInstance().reference.child("Items")
                     .child(getRef(position).key!!).updateChildren(map)
                     .addOnSuccessListener {
                         Toast.makeText(
-                            holder.itemName.context,
-                            "Data Updated successfully!",
+                            holder.name.context,
+                            "Data updated successfully",
                             Toast.LENGTH_SHORT
                         ).show()
                         dialogPlus.dismiss()
                     }
                     .addOnFailureListener {
                         Toast.makeText(
-                            holder.itemName.context,
+                            holder.name.context,
                             "Error while updating",
                             Toast.LENGTH_SHORT
                         ).show()
@@ -85,23 +93,26 @@ class MainAdapter
                     }
             }
         }
+
+        // Initialize the btnDelete button after the view holder is fully inflated.
         holder.btnDelete.setOnClickListener {
-            val builder = AlertDialog.Builder(holder.itemName.context)
+            val builder = AlertDialog.Builder(holder.name.context)
             builder.setTitle("Are you sure?")
-            builder.setMessage("Deleted data can't be Undo.")
+            builder.setMessage("Delete data cannot be undo.")
             builder.setPositiveButton("Delete") { dialogInterface, which ->
                 FirebaseDatabase.getInstance().reference.child("Items")
                     .child(getRef(position).key!!).removeValue()
             }
-            builder.setNegativeButton("Cancel") { dialogInterface, which ->
+            builder.setNegativeButton("Cancel") { dialog, which ->
                 Toast.makeText(
-                    holder.itemName.context,
-                    "Cancel",
+                    holder.name.context,
+                    "Cancelled",
                     Toast.LENGTH_SHORT
                 ).show()
             }
             builder.show()
         }
+        holder.btnEdit.visibility = View.VISIBLE
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
@@ -109,9 +120,10 @@ class MainAdapter
         return myViewHolder(view)
     }
 
+
     inner class myViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var img: CircleImageView
-        var itemName: TextView
+        var name: TextView
         var price: TextView
         var description: TextView
         var btnEdit: Button
@@ -119,7 +131,7 @@ class MainAdapter
 
         init {
             img = itemView.findViewById<View>(R.id.img1) as CircleImageView
-            itemName = itemView.findViewById<View>(R.id.itemNametext) as TextView
+            name = itemView.findViewById<View>(R.id.itemNametext) as TextView
             price = itemView.findViewById<View>(R.id.pricetext) as TextView
             description = itemView.findViewById<View>(R.id.descriptiontext) as TextView
             btnEdit = itemView.findViewById<View>(R.id.btnEdit) as Button
