a81b206e88e883bd9f461e050a8019fc6a1abc62
Kavindu Naveen <it21226878@my.sliit.lk>
2023-05-16 14:59:23 +0530

    kavindu

--- app/src/main/java/com/example/crudmyapp/AddActivity.kt
+++ app/src/main/java/com/example/crudmyapp/AddActivity.kt
@@ -9,47 +9,41 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 
+
+// Class declaration
 class AddActivity : AppCompatActivity() {
     var itemName: EditText? = null
     var price: EditText? = null
     var description: EditText? = null
-    var iImage: EditText? = null
     var btnAdd: Button? = null
     var btnBack: Button? = null
+
+    // Method called when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
 
 
-       /* var btnBack = findViewById<Button>(R.id.btnBack)
-        btnBack.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-
-
-        }
-
-        */
-
+        // Initializing variables
         itemName = findViewById<View>(R.id.txtItemName) as EditText
         price = findViewById<View>(R.id.txtPrice) as EditText
         description = findViewById<View>(R.id.txtDescription) as EditText
-        iImage = findViewById<View>(R.id.txtImageUrl) as EditText
         btnAdd = findViewById<View>(R.id.btnAdd) as Button
         btnBack = findViewById<View>(R.id.btnBack) as Button
         btnAdd!!.setOnClickListener {
             insertData()
             clearAll()
         }
+        // Set click listener for the "Back" button
         btnBack!!.setOnClickListener { finish() }
     }
 
+    // Method to insert data into Firebase database
     private fun insertData() {
         val map: MutableMap<String, Any> = HashMap()
         map["itemName"] = itemName!!.text.toString()
         map["price"] = price!!.text.toString()
         map["description"] = description!!.text.toString()
-        map["iImage"] = iImage!!.text.toString()
         FirebaseDatabase.getInstance().reference.child("Items").push()
             .setValue(map)
             .addOnSuccessListener {
@@ -67,11 +61,10 @@ class AddActivity : AppCompatActivity() {
                 ).show()
             }
     }
-
+    // Method to clear all input fields
     private fun clearAll() {
         itemName!!.setText("")
         price!!.setText("")
         description!!.setText("")
-        iImage!!.setText("")
     }
 }
