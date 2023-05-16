93d64cc64f396e3f99fd699c4c53b48832ef4166
chameenshamika <chameenshamika@gmail.com>
2023-05-16 14:44:19 +0530

    comment sign_in.kt

--- app/src/main/java/com/example/myapp/sign_in.kt
+++ app/src/main/java/com/example/myapp/sign_in.kt
@@ -16,31 +16,39 @@ class sign_in : AppCompatActivity() {
         binding = SignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Get an instance of FirebaseAuth
         firebaseAuth = FirebaseAuth.getInstance()
+        // Set click listener for "textView" element
         binding.textView.setOnClickListener {
+            // Create an intent to navigate to the sign_up activity
             val intent = Intent(this, sing_up::class.java)
             startActivity(intent)
         }
 
+        // Set click listener for "button" element
         binding.button.setOnClickListener {
+            // Retrieve email and password from input fields
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
 
-
+            // Check if email and password are not empty
             if (email.isNotEmpty() && pass.isNotEmpty()) {
 
-
+                // Sign in with email and password using FirebaseAuth
                 firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
+                        // Sign-in successful, navigate to home_page activity
                         val intent = Intent(this, home_page::class.java)
                         startActivity(intent)
                     } else {
+                        // Sign-in failed, display the exception message as a toast
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
                     }
                 }
 
             } else {
+                // Display a toast if any of the fields are empty
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
 
             }
